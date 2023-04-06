1d5a2da523ce1b27a7cf3267bfca1a6c14656ba4
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-04-05 21:52:51 -0500

    test: created additional tests; added code to handle invalid numbers of dice

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 
 
 class GameLogic:
@@ -10,8 +11,7 @@ class GameLogic:
             :param n: number of dice to be rolled
             :return: tuple containing the six results
         """
-        results = tuple(random.randint(1, 6) for _ in range(n))
-        return results
+        return tuple(random.randint(1, 6) for _ in range(n))
 
     @staticmethod
     def calculate_score(dice_roll):
@@ -20,3 +20,22 @@ class GameLogic:
             :param dice_roll: tuple representing a number of dice being rolled
             :return: integer representing the total score
         """
+        if not len(dice_roll) or len(dice_roll) > 6:
+            return 0
+        dice_counter = Counter(dice_roll)
+        distinct_dice = len(dice_counter)
+        most_common_roll_count = dice_counter.most_common(1)[0][1]
+        if distinct_dice == 6:
+            return 1500
+        if distinct_dice == 3 and most_common_roll_count == 2:
+            return 1500
+        roll_score = 0
+        base_scores = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
+        for number_on_dice, frequency in dice_counter.items():
+            if frequency >= 3:
+                roll_score += base_scores[number_on_dice] * (frequency - 2)
+            elif number_on_dice == 1:
+                roll_score += 100 * frequency
+            elif number_on_dice == 5:
+                roll_score += 50 * frequency
+        return roll_score
